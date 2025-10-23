4297dcb50a91fdc378ed5d9a8580d28e18da3530
Vivien Márton <vivien.marton@spsehalova.sk>
2025-10-23 19:08:48 +0200

    add cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from database import Base, engine
 from routers import user, auth, post
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 
@@ -11,6 +12,21 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:8100",
+    "http://56.228.35.186",
+    "https://56.228.35.186",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router, prefix="/api")
 app.include_router(user.router, prefix="/api")
 app.include_router(post.router, prefix="/api")
